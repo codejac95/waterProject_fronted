137805ea091c6402f2f7929ed285a2aa20c363f1
Jacob Jansson <jacob950422@gmail.com>
2024-09-28 22:03:38 +0200

    gifs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import './app.css'
-import wateringGif from './img/watering.gif';
+import moistureGif from './img/moisture.gif';
 import tempatureGif from './img/tempature.gif';
 import humidityGif from './img/humidity.gif';
+import greenhouse from './img/greenhouse.png';
 
-const TemperatureHumidityFetcher = () => {
+const TemperatureHumidityMoistureFetcher = () => {
   const [temperature, setTemperature] = useState<number | null>(null);
   const [humidity, setHumidity] = useState<number | null>(null);
   const [moisture, setMoisture] = useState<number | null>(null);
@@ -14,8 +15,8 @@ const TemperatureHumidityFetcher = () => {
       if (!response.ok) {
         throw new Error('Failed to fetch temperature');
       }
-      const temp: number = await response.json();
-      setTemperature(temp);
+      const temperature: number = await response.json();
+      setTemperature(temperature);
   };
 
   const fetchHumidity = async () => {
@@ -23,17 +24,17 @@ const TemperatureHumidityFetcher = () => {
       if (!response.ok) {
         throw new Error('Failed to fetch humidity');
       }
-      const hum: number = await response.json();
-      setHumidity(hum);
+      const humidity: number = await response.json();
+      setHumidity(humidity);
   };
 
   const fetchMoisture = async () => {
     const response = await fetch('http://localhost:8080/getMoisture');
     if (!response.ok) {
-      throw new Error('Failed to fetch humidity');
+      throw new Error('Failed to fetch moisture');
     }
-    const moi: number = await response.json();
-    setMoisture(moi);
+    const moisture: number = await response.json();
+    setMoisture(moisture);
 };
 
   useEffect(() => {
@@ -42,33 +43,53 @@ const TemperatureHumidityFetcher = () => {
       await fetchHumidity();
       await fetchMoisture();
     };
-
     fetchData();
     const intervalid = setInterval(fetchData,30000);
     return () => clearInterval(intervalid);
     },[]);
 
-  return (
-    <div>
-      <h1>Temperature, Humidity & Soil moisture Data</h1>
-      <div id = "temperature">
-       <p>Temperature: {temperature} °C
-       <img src={tempatureGif} alt="temperature"
-        style={{ marginLeft: '10px', width: '50px', height: '50px' }} /></p>
+    return (
+      <div className="container">
+        <div className="data-section">
+        <h2>Greenhouse data:</h2>
+        <p>(if anything blinks, it means trouble)</p>
+          <div id="temperature">
+            <p>
+              Temperature: {temperature} °C
+              <img
+                src={tempatureGif}
+                alt="temperature"
+                className={temperature && (temperature < 15 || temperature > 30) ? 'blink' : ''}
+                style={{width: '50px', height: '50px' }}
+              />
+            </p>
+          </div>
+          <div id="humidity">
+            <p>
+              Humidity: {humidity} %
+              <img
+                src={humidityGif}
+                alt="humidity"
+                className={humidity && (humidity < 60 || humidity > 80) ? 'blink' : ''}
+                style={{width: '60px', height: '60px' }}
+              />
+            </p>
+          </div>
+          <div id="moisture">
+            <p>
+              Soil moisture: {moisture} %
+              <img
+                src={moistureGif}
+                alt="moisture"
+                className={moisture && (moisture < 20 || moisture > 50) ? 'blink' : ''}
+                style={{width: '60px', height: '60px' }}
+              />
+            </p>
+          </div>
+        </div>
+        <img id="greenhouse" src={greenhouse} alt="greenhouse" />
       </div>
-      <div id = "humidity">
-        <p>Humidity: {humidity} %
-        <img src={humidityGif} alt="humidity"
-        style={{ marginLeft: '10px', width: '50px', height: '50px' }} /></p>
-      </div>
-      <div id = "moisture">
-        <p>Soil moisture: {moisture} % 
-        <img src={wateringGif} alt="watering"
-        style={{ marginLeft: '10px', width: '50px', height: '50px' }} /></p>
-      </div>
-     
-    </div>
-  );
-};
+    );
+  };
 
-export default TemperatureHumidityFetcher;
+export default TemperatureHumidityMoistureFetcher;
